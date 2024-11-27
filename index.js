52c80cb1a88224d525bb3195eb58e1ac5184447f
salvaged the warm lord <144842088+salvagedtheswarmlord@users.noreply.github.com>
2024-11-27 11:27:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+let userName:'';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+const userQuestion = 'Will I have a good day today?';
+console.log(`${userName ? userName : 'User'} asked: ${userQuestion}`);
+const randomNumber=Math.floor(Math.random() *8);
+let eightBall='';
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+console.log(eightBall);
+if (randomNumber === 0) {
+  eightBall = 'It is certain';
+} else if (randomNumber === 1) {
+  eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+  eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+  eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+  eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+  eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+  eightBall = 'Signs point to yes';
+}
